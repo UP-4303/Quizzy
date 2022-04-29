6a2b0cd3d920e908176f48a762ec0f144a068e2c
UP-4303 <up4303.pro@protonmail.com>
2022-04-29 11:40:15 +0200

    style

--- lib/top_bar.php
+++ lib/top_bar.php
@@ -1,6 +1,6 @@
 <header>
-		<div id="top_container">
-			<img src="images/logo.png" alt="logo" id="logo">
-			<div id="top_bar"></div>
+		<div id="top_container" class="no_margin">
+			<img src="images/logo.png" alt="logo" id="logo" class="no_margin">
+			<div id="top_bar" class="no_margin"></div>
 		</div>
 </header>
