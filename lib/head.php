171f6a911e824cd08853f69d9e10f841f5f38037
UP-4303 <up4303.pro@protonmail.com>
2022-04-29 15:14:52 +0200

    style

--- lib/head.php
+++ lib/head.php
@@ -6,14 +6,14 @@
     <link rel="stylesheet" href="css/style.css">
     <script src="js/script.js"></script>
 </head>
-<body class="no_margin">
+<body class="no_margin no_padding">
  
     <?php
     include("db/db_connect.php") ;
     ?>
     <header>
         <div id="top_container" class="no_margin">
-            <img src="images/logo.png" alt="logo" id="logo" class="no_margin">
+            <img src="images/logo.png" alt="logo" id="logo">
             <div id="top_bar" class="no_margin"></div>
         </div>
     </header>
