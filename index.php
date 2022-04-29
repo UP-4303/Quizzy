d79621dd87deb17f4858564ae0d928146b97531f
Xavier Maziere <mazierex@formation.univ-smb.fr>
2022-04-29 10:04:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<p>Hello world</p>
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <link rel="stylesheet" href="">
+  <script src=""></script>
+ </head>
+ <body>
+
+	<p>Hello world</p>
+
+ </body>
+</html>
