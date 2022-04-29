a73ccd36b215ae771b7c1c4c4a2d04cd3da16d1a
Xavier Maziere <mazierex@formation.univ-smb.fr>
2022-04-29 11:51:17 +0200

    fix link

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include("lib/head.php");
 include("lib/top_bar.php");
 ?>
 
-<a href="user/user_connect.php">connexion</a>
+<a href="user_connect.php">connexion</a>
 
 <?php
 include("lib/foot.php");
