f3cbcfca9768f40ce80eb963d1bc2e1580f8a1ba
UP-4303 <up4303.pro@protonmail.com>
2022-05-09 15:42:14 +0200

    Progéniture de catin

--- lib/head.php
+++ lib/head.php
@@ -15,7 +15,7 @@
     <header>
         <div id="top_container" class="no_margin no_padding">
             <div id="logo_wrapper">
-                <a href="/l1_info_4/Quizzy" id="logo_link"><img src="images/logo.png" alt="logo" id="logo"></a>
+                <a href="/l1_info_4/Quizzy" id="logo_link" class="no_margin no_padding"><img src="images/logo.png" alt="logo" id="logo"></a>
             </div>
             <div id="searchbox_wrapper">
                 <input id="searchbox" type="text" class="form-input" placeholder="Chercher un quizz" name="search" title="Rechercher">
