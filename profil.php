e0731f272d2e1bef2afe23dafd10837ec8b7de59
Osteriux <pixave73@gmail.com>
2022-05-11 16:22:21 +0200

    update profil

--- profil.php
+++ profil.php
@@ -7,9 +7,20 @@ include_once("crud/users.crud.php");
 
 $user = select_user($conn, 14) ;
 
-foreach($user as $key => $value) {
-	echo $key." => ".$value."\n" ;
+$info = "<ul>\n" ;
+$info .= "\t<li>pseudo : ".$user["nickname"]."</li>\n" ;
+$info .= "\t<li>email : ".$user["email"]."</li>\n" ;
+$info .= "</ul>\n>" ;
+
+echo $info ;
+
+if(isset($user["profile_picture"])) {
+	$image = $user["profile_picture"] ;
+} else {
+	$image = "default-avatar.jpb" ;
 }
+
+echo "<img src='images/".$image."' alt='profil picture' />" ;
 
 ?>
 
