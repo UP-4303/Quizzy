71c457bdf2b07ff3794c8682718ab64d0195aa9e
UP-4303 <up4303.pro@protonmail.com>
2022-05-11 18:52:38 +0200

    azaaae gdh

--- lib/head.php
+++ lib/head.php
@@ -27,7 +27,7 @@
     include_once("db/db_connect.php") ;
     ?>
     <header id="header">
-    <svg id="top_svg"><path fill="#fc6600" fill-opacity="1" d="M 0 0 L 800 0 L 800 150 L 350 150 C 200 150 250 200 100 200 L 0 200 Z" id="top_path" class="no_margin no_padding"></path></svg>
+    <svg id="top_svg" class="no_margin no_padding"><path fill="#fc6600" fill-opacity="1" d="M 0 0 L 800 0 L 800 150 L 350 150 C 200 150 250 200 100 200 L 0 200 Z"></path></svg>
         <div id="top_container">
             <div id="logo_wrapper">
                 <a href="/l1_info_4/Quizzy" id="logo_link" class="no_margin no_padding"><img src="images/logo.png" alt="logo" id="logo"></a>
@@ -39,7 +39,8 @@
                 </lord-icon>
             </div>
             <div id="searchbox_wrapper">
-                <input id="searchbox" type="text" class="form-input" placeholder="Chercher un quizz" name="search" title="Rechercher">
+                <input id="searchbox" class="no_padding" 
+                type="text" class="form-input" placeholder="Chercher un quizz" name="search" title="Rechercher">
                 <lord-icon id="searchbox_icon"
                     src="https://cdn.lordicon.com/msoeawqm.json"
                     trigger="morph"
