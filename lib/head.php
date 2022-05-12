168845d662d44e6bb410197c03a52c80ab450a02
Osteriux <pixave73@gmail.com>
2022-05-12 17:13:14 +0200

    fix menu 8

--- lib/head.php
+++ lib/head.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <title>Quizzy</title>
     <link rel="stylesheet" href="css/style.css">
-    <script src="js/script.js"></script>
     <script src="https://cdn.lordicon.com/xdjxvujz.js"></script>
 </head>
 <body class="no_margin no_padding">
@@ -90,3 +89,4 @@
 		
 
     </header>
+    <script src="js/script.js"></script>
