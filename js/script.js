83dc76c851e2c6b8ef6224950e03d799d32a3dee
Osteriux <pixave73@gmail.com>
2022-05-12 17:17:15 +0200

    fix menu 9

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 var iconeMenu = document.querySelector(".iconeMenu");
+var menu_col = document.querySelector(".menu");
 var menu = "close";
 
 iconeMenu.addEventListener("click", clicMenu);
@@ -6,9 +7,11 @@ iconeMenu.addEventListener("click", clicMenu);
 function clicMenu() {
 	if (menu == "close") {
 		iconeMenu.className = "iconeMenu afficheMenu";
+		menu_col.className = "menu menu_oui";
 		menu = "open";
 	} else {
 		iconeMenu.className = "iconeMenu";
+		menu_col.className = "menu menu_non";
 		menu = "close";
 	}
 }
