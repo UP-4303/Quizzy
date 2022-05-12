0d0bc663e32001c6a5ab6394b44c08d1d3261561
Osteriux <pixave73@gmail.com>
2022-05-12 16:28:19 +0200

    up menu

--- lib/head.php
+++ lib/head.php
@@ -64,6 +64,9 @@
 							$image = "default-avatar.jpg" ;
 						}
 						echo "<div id='top_menu_co'><div id='cont_pic_menu'><img class='profil_pic' src='images/".$image."' alt='profil picture' ></div><h4>Connecté en tant que : ".$user["nickname"]."</h4>" ;
+						echo "<a href='#'>Collection</a>" ;
+						echo "<a href='#'>Mes créations</a>" ;
+						echo "<a href='#'>Mes derniers Quizz</a>" ;
 						if ($_SESSION['admin']){
 							echo "<a href='admin/index.php'>Accéder au dark web</a></div>" ;
 						}
