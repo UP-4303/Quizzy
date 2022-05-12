63c8a996dbca599680873d159e2ac31df7bc54b3
Xados <jxds.lph@pm.me>
2022-05-12 16:34:54 +0200

    Update quizz.php

--- quizz.php
+++ quizz.php
@@ -4,14 +4,14 @@ include("lib/head.php");
 session_start();
 if(isset($_SESSION['id'])){
 	if (isset($_GET["quizz"])){
-		$id=$_GET['quizz']
+		$id=$_GET['quizz'];
 	}else{
 		print("Aucun quizz n'a été chargé.");
 	}
 }else{
 	print("Veuillez vous connecter pour accéder aux quizz.");
 	include("lib/foot.php");
-	return
+	return;
 }
 ?>
 
