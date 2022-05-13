1ee1e7cd4d04005368560d9cbe874c6a48a80457
Xados <jxds.lph@pm.me>
2022-05-13 13:54:25 +0200

    Page de création

---
+++ creation.php
@@ -0,0 +1,25 @@
+<?php
+include("lib/head.php");
+include_once("crud/quizz.crud.php");
+if(! isset($_SESSION['id'])){
+	print("Veuillez vous connecter pour accéder aux quizz.");
+	include("db/db_disconnect.php") ;
+	return;
+}
+?>
+
+
+<form method="POST" action="creation.php">
+	<label id="nametext">Nom : </label><input id="nameinput" type="text" name="nom">
+	<input id="qcminput" type="radio" name="is_quizz" checked><label id="qcmtext">Quizz à points</label>
+	<input id="quizzinput" type="radio" name="is_quizz"><label id="quizztext">Quizz à résultats multiples</label>
+
+	<button id="continue"></button>
+</form>
+
+
+<script src="creation.js"></script>
+
+<?php
+	include("db/db_disconnect.php") ;
+?>
