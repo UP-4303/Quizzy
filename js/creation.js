fcdda46b72de40356bd7d737412384d30442c313
Xados <jxds.lph@pm.me>
2022-05-16 22:14:00 +0200

    letzgo

--- js/creation.js
+++ js/creation.js
@@ -7,7 +7,7 @@ var is_quizz = false
 
 function isquizz(){
 	console.log("update")
-	var is_quizz = quizz_input.checked
+	is_quizz = quizz_input.checked
 }
 
 initform.addEventListener("click", isquizz)
@@ -32,7 +32,7 @@ function updateLabels(){
 		div.getElementsByClassName("qnumber")[0].textContent = `Question ${nb}`
 		if (is_quizz){
 			div.getElementsByClassName("jauges")[0].style.display = ""
-			div.getElementsByClassName("points")[0];style.display = "none"
+			div.getElementsByClassName("points")[0].style.display = "none"
 		}else{
 			div.getElementsByClassName("jauges")[0].style.display = "none"
 			div.getElementsByClassName("points")[0].style.display = ""
