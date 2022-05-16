c50d4fb2b54b681d5416fa86a982f218a7a55063
Xados <jxds.lph@pm.me>
2022-05-16 13:42:13 +0200

    Update creation.php

--- creation.php
+++ creation.php
@@ -9,33 +9,29 @@ if(! isset($_SESSION['id'])){
 ?>
 
 
-<form method="POST" action="creation.php">
-	<div id="initform">
-		<label id="nametext">Nom : </label><input id="nameinput" type="text" name="nom">
-		<input id="qcminput" type="radio" name="is_quizz" checked><label for="qcminput" id="qcmtext">Quizz à points</label>
-		<input id="quizzinput" type="radio" name="is_quizz"><label for="quizzinput" id="quizztext">Quizz à résultats multiples</label>
-	</div>
-
-	<!--<div id="resultsform" style="display: none;">
-		<label>Configuration des résultats</label>
-		<input type="number" min="0" max="20" name="">
-	</div> -->
-
-	<div id="qform1" style="display: none;">
-		<label id="question_number">Question</label><br/>
-		<label>Question : </label><input type="text" name="questiontext"><br/>
-		<select id="ans_number">
-			<option value="2">2 réponses possibles</option>
-			<option value="3">3 réponses possibles</option>
-			<option value="4">4 réponses possibles</option>
-		</select><br/>
-		<label id="reptext1">Réponse 1 : </label><input type="text" name="repinput1" id="repinput1"><br/>
-		<label id="reptext2">Réponse 2 : </label><input type="text" name="repinput2" id="repinput2"><br/>
-		<label id="reptext3" style="display: none;">Réponse 3 : </label><input type="text" name="repinput3" id="repinput3" style="display: none;"><br/>
-		<label id="reptext4" style="display: none;">Réponse 4 : </label><input type="text" name="repinput4" id="repinput4" style="display: none;"><br/>
-		<button class="addmore">Ajouter une question</button>
-	</div>
-</form>
+<div id="initform">
+	<label id="nametext">Nom : </label><input id="nameinput" type="text" name="nom">
+	<input id="qcminput" type="radio" name="is_quizz" checked><label for="qcminput" id="qcmtext">Quizz à points</label>
+	<input id="quizzinput" type="radio" name="is_quizz"><label for="quizzinput" id="quizztext">Quizz à résultats multiples</label>
+</div>
+<!--<div id="resultsform" style="display: none;">
+	<label>Configuration des résultats</label>
+	<input type="number" min="0" max="20" name="">
+</div> -->
+<div id="qform1" style="display: none;">
+	<label id="question_number">Question</label><br/>
+	<label>Question : </label><input type="text" name="questiontext"><br/>
+	<select id="ans_number">
+		<option value="2">2 réponses possibles</option>
+		<option value="3">3 réponses possibles</option>
+		<option value="4">4 réponses possibles</option>
+	</select><br/>
+	<label id="reptext1">Réponse 1 : </label><input type="text" name="repinput1" id="repinput1"><br/>
+	<label id="reptext2">Réponse 2 : </label><input type="text" name="repinput2" id="repinput2"><br/>
+	<label id="reptext3" style="display: none;">Réponse 3 : </label><input type="text" name="repinput3" id="repinput3" style="display: none;"><br/>
+	<label id="reptext4" style="display: none;">Réponse 4 : </label><input type="text" name="repinput4" id="repinput4" style="display: none;"><br/>
+	<button class="addmore">Ajouter une question</button>
+</div>
 
 <button id="continue">Suivant</button>
 
