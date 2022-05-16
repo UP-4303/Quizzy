c63f58d9f27a260c94cb8b360bc4d4df2d77fe99
Xados <jxds.lph@pm.me>
2022-05-16 13:24:42 +0200

    j'ai jamais push ça

--- creation.php
+++ creation.php
@@ -16,22 +16,27 @@ if(! isset($_SESSION['id'])){
 		<input id="quizzinput" type="radio" name="is_quizz"><label for="quizzinput" id="quizztext">Quizz à résultats multiples</label>
 	</div>
 
+	<div id="resultsform" style="display: none;">
+		<label>Configuration des résultats</label>
+		<input type="number" min="0" max="20" name="">
+	</div>
+
 	<div id="questionform" style="display: none;">
 		<label id="question_number">Question</label><br/>
 		<label>Question : </label><input type="text" name="questiontext"><br/>
-		<select id="ans_number" value="4">
+		<select id="ans_number">
 			<option value="2">2 réponses possibles</option>
 			<option value="3">3 réponses possibles</option>
 			<option value="4">4 réponses possibles</option>
 		</select><br/>
 		<label id="reptext1">Réponse 1 : </label><input type="text" name="repinput1" id="repinput1"><br/>
 		<label id="reptext2">Réponse 2 : </label><input type="text" name="repinput2" id="repinput2"><br/>
-		<label id="reptext3">Réponse 3 : </label><input type="text" name="repinput3" id="repinput3"><br/>
-		<label id="reptext4">Réponse 4 : </label><input type="text" name="repinput4" id="repinput4"><br/>
+		<label id="reptext3" style="display: none;">Réponse 3 : </label><input type="text" name="repinput3" id="repinput3" style="display: none;"><br/>
+		<label id="reptext4" style="display: none;">Réponse 4 : </label><input type="text" name="repinput4" id="repinput4" style="display: none;"><br/>
 	</div>
 </form>
 
-<button id="continue" value="Suivant"></button>
+<button id="continue">Suivant</button>
 
 <script src="js/creation.js"></script>
 
