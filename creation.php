aed29b5382d219cd72b28dfac0f97b231df7582c
Xados <jxds.lph@pm.me>
2022-05-17 00:26:58 +0200

    frgdfg

--- creation.php
+++ creation.php
@@ -1,70 +1,89 @@
 <?php
-#include("lib/head.php");
-#include_once("crud/quizz.crud.php");
+include("lib/head.php");
+include_once("crud/quizz.crud.php");
 ?>
 
 <div class="form_wrapper">
-<div id="initform">
-	<input id="qcminput" type="radio" name="is_quizz" checked><label for="qcminput" id="qcmtext">Quizz à points</label>
-	<input id="quizzinput" type="radio" name="is_quizz"><label for="quizzinput" id="quizztext">Quizz à résultats multiples</label>
+	<form method="POST" action="creation.php"></form>
+		<div id="initform">
+			<input id="qcminput" type="radio" name="is_quizz" checked><label for="qcminput" id="qcmtext">Quizz à points</label>
+			<input id="quizzinput" type="radio" name="is_quizz"><label for="quizzinput" id="quizztext">Quizz à résultats multiples</label>
+		</div>
+		<div id="qcmform">
+			<label>Nombre de points minimum pour réussir le test : </label><input type="number" name="minpoints">
+		</div>
+		<div id="quizzform" style="display: none;">
+			<label>Nom de la jauge A : </label><input type="text" name="A"><br/>
+			<label>Nom de la jauge B : </label><input type="text" name="B"><br/>
+			<label>Nom de la jauge C : </label><input type="text" name="C"><br/>
+			<label>Nom de la jauge D : </label><input type="text" name="D">
+		</div>
+		<div id="qwrapper">
+			<div class="qform" id="qform1">
+				<label class="qnumber">Question 1</label><br/>
+				<label>Question : </label><input type="text" name="questiontext"><br/>
+				<select class="ansnumber">
+					<option value="2">2 réponses possibles</option>
+					<option value="3">3 réponses possibles</option>
+					<option value="4">4 réponses possibles</option>
+				</select><br/>
+				
+				<div class="ansinput">
+					<label>Réponse 1 : </label>
+					<input type="text" name="repinput1" >
+					<select class="jauges" style="display: none;">
+						<option value="A">Jauge A</option>
+						<option value="B">Jauge B</option>
+						<option value="C">Jauge C</option>
+					<option value="D">Jauge D</option>
+					</select>
+					<input type="number" min="-2" max="2" value="0" name="points" class="points"><br/>
+				</div>
+				<div class="ansinput">
+					<label>Réponse 2 : </label>
+					<input type="text" name="repinput2" >
+					<select class="jauges" style="display: none;">
+						<option value="A">Jauge A</option>
+						<option value="B">Jauge B</option>
+						<option value="C">Jauge C</option>
+						<option value="D">Jauge D</option>
+					</select>
+					<input type="number" min="-2" max="2" value="0" name="points" class="points"><br/>
+				</div>
+				<div class="ansinput" style="display: none;">
+					<label>Réponse 3 : </label>
+					<input type="text" name="repinput3" >
+					<select class="jauges" style="display: none;">
+						<option value="A">Jauge A</option>
+						<option value="B">Jauge B</option>
+						<option value="C">Jauge C</option>
+						<option value="D">Jauge D</option>
+					</select>
+					<input type="number" min="-2" max="2" value="0" name="points" class="points"><br/>
+				</div>
+				<div class="ansinput" style="display: none;">
+					<label>Réponse 4 : </label>
+					<input type="text" name="repinput4" >
+					<select class="jauges" style="display: none;">
+						<option value="A">Jauge A</option>
+						<option value="B">Jauge B</option>
+						<option value="C">Jauge C</option>
+						<option value="D">Jauge D</option>
+					</select>
+					<input type="number" min="-2" max="2" value="0" name="points" class="points"><br/>
+				</div>
+				<button onclick="addmore(this)">Ajouter une question</button>
+				<button onclick="remove(this)">Retirer cette question</button>
+				<br/>
+			</div>
+		</div>
+	<input type="submit" name="Valider et créer">
+	</form>
 </div>
-<div id="qcmform">
-	<label>Nombre de points minimum pour réussir le test : </label><input type="number" name="minpoints">
-</div>
-<div id="quizzform" style="display: none;">
-	<label>Nom de la jauge A : </label><input type="text" name="A"><br/>
-	<label>Nom de la jauge B : </label><input type="text" name="B"><br/>
-	<label>Nom de la jauge C : </label><input type="text" name="C"><br/>
-	<label>Nom de la jauge D : </label><input type="text" name="D">
-</div>
-<div id="qwrapper"><div class="qform" id="qform1">
-		<label class="qnumber">Question 1</label><br/>
-		<label>Question : </label><input type="text" name="questiontext"><br/>
-		<select class="ansnumber">
-			<option value="2">2 réponses possibles</option>
-			<option value="3">3 réponses possibles</option>
-			<option value="4">4 réponses possibles</option>
-		</select><br/>
-		
-		<div class="ansinput"><label>Réponse 1 : </label><input type="text" name="repinput1" ><select class="jauges" style="display: none;">
-			<option value="A">Jauge A</option>
-			<option value="B">Jauge B</option>
-			<option value="C">Jauge C</option>
-			<option value="D">Jauge D</option>
-		</select>
-		<input type="number" min="-2" max="2" value="0" name="points" class="points"><br/></div>
-		<div class="ansinput"><label>Réponse 2 : </label><input type="text" name="repinput2" ><select class="jauges" style="display: none;">
-			<option value="A">Jauge A</option>
-			<option value="B">Jauge B</option>
-			<option value="C">Jauge C</option>
-			<option value="D">Jauge D</option>
-		</select>
-		<input type="number" min="-2" max="2" value="0" name="points" class="points"><br/></div>
-		<div class="ansinput" style="display: none;"><label>Réponse 3 : </label><input type="text" name="repinput3" ><select class="jauges" style="display: none;">
-			<option value="A">Jauge A</option>
-			<option value="B">Jauge B</option>
-			<option value="C">Jauge C</option>
-			<option value="D">Jauge D</option>
-		</select>
-		<input type="number" min="-2" max="2" value="0" name="points" class="points"><br/></div>
-		<div class="ansinput" style="display: none;"><label>Réponse 4 : </label><input type="text" name="repinput4" ><select class="jauges" style="display: none;">
-			<option value="A">Jauge A</option>
-			<option value="B">Jauge B</option>
-			<option value="C">Jauge C</option>
-			<option value="D">Jauge D</option>
-		</select>
-		<input type="number" min="-2" max="2" value="0" name="points" class="points"><br/></div>
-		<button onclick="addmore(this)">Ajouter une question</button>
-		<button onclick="remove(this)">Retirer cette question</button>
-		<br/>
-</div></div>
-</div>
-
-<button id="continue">Suivant</button>
 
 
 <script src="js/creation.js"></script>
 
 <?php
-	#include("db/db_disconnect.php") ;
+	include("db/db_disconnect.php") ;
 ?>
--- js/creation.js
+++ js/creation.js
@@ -1,4 +1,3 @@
-var next = document.getElementById("continue")
 var qform = document.getElementById("qform1")
 var qwrapper = document.getElementById("qwrapper")
 var quizz_input = document.getElementById("quizzinput")
@@ -54,39 +53,3 @@ function displayAnswers(evt){
 		c++
 	}
 }
-
-
-
-/*var quizz_input = document.getElementById("quizzinput")
-var qnumberhtml = document.getElementById("question_number")
-var initform = document.getElementById("initform")
-var questionform = document.getElementById("questionform")
-var buttoncontinue = document.getElementById("continue")
-var reptext3 = document.getElementById("reptext3")
-var reptext4 = document.getElementById("reptext4")
-var repinput3 = document.getElementById("repinput3")
-var repinput4 = document.getElementById("repinput4")
-
-var q = []
-var results = {}
-var q_number = 1
-
-function displayAnswers(){
-	var nb = parseInt(ansnumber.value)
-	reptext3.style.display = nb>2?"":"none"
-	repinput3.style.display = nb>2?"":"none"
-	reptext4.style.display = nb>3?"":"none"
-	repinput4.style.display = nb>3?"":"none"
-}
-
-var ansnumber = document.getElementById("ans_number")
-ansnumber.addEventListener("input", displayAnswers)
-
-function displayContinue(){
-	initform.style.display = "none"
-	questionform.style.display = ""
-	qnumberhtml.innerHTML = `Question ${q_number}`
-	var is_quizz = quizz_input.checked
-}
-
-buttoncontinue.onclick = displayContinue*/
