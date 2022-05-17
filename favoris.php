dac68d5a2d909ee762d781865940fb7d88302f7e
Osteriux <pixave73@gmail.com>
2022-05-17 13:58:12 +0200

    fix favoris

--- favoris.php
+++ favoris.php
@@ -14,7 +14,7 @@ include("lib/head.php");
 		echo("<div id='quizz_name'>aucun favoris</div>");
 	} else {
 		foreach($favoris as $fav_id) {
-			$qizz = select_quizz($conn, int($fav_id)) ;
+			$qizz = select_quizz($conn, intval($fav_id)) ;
 			if(isset($qizz["image"])) {
 				$background = 'background-image: url("images/'.$qizz["image"].'");' ;
 			} else {
