2d9d662ff3b25e781a745f3091039b5b5045fb75
Osteriux <pixave73@gmail.com>
2022-05-17 13:35:13 +0200

    fix favoris

--- favoris.php
+++ favoris.php
@@ -6,15 +6,15 @@ include("lib/head.php");
 	<?php
 	include_once("crud/quizz.crud.php");
 	
-	$user = select_user($conn, $_SESSION["id"])
+	$user = select_user($conn, $_SESSION["id"]) ;
 	
-	$favoris = $user["favoris"]
+	$favoris = $user["favoris"] ;
 	
 	if (mysqli_num_rows($favoris) == 0){
 		echo("<div id='quizz_name'>aucun favoris</div>");
 	} else {
 		foreach($favoris as $fav_id) {
-			$qizz = select_quizz($conn, $fav_id)
+			$qizz = select_quizz($conn, $fav_id) ;
 			if(isset($qizz["image"])) {
 				$background = 'background-image: url("images/'.$qizz["image"].'");' ;
 			} else {
