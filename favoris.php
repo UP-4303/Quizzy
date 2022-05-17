6bea2d9193ffdb4b207734c857665277a695e732
Osteriux <maziere.xavier@gmail.com>
2022-05-17 15:14:06 +0200

    fix si pas co

--- favoris.php
+++ favoris.php
@@ -4,26 +4,30 @@ include("lib/head.php");
 
 <ul class="list_quizz">
 	<?php
-	include_once("crud/quizz.crud.php");
-	
-	$user = select_user($conn, $_SESSION["id"]) ;
-	
-	$favoris = explode('.', $user["favoris"]) ;
-	
-	if (count($favoris) < 2){
-		echo("<div id='quizz_name'>vous n'avez aucun favoris</div>");
-	} else {
-		foreach($favoris as $fav_id) {
-			if($fav_id != '') {
-				$qizz = select_quizz($conn, intval($fav_id)) ;
-				if(isset($qizz["image"])) {
-					$background = 'background-image: url("images/'.$qizz["image"].'");' ;
-				} else {
-					$background = "background-color: ".$qizz["color"].";";
+	if(isset($_SESSION["id"])) {
+		include_once("crud/quizz.crud.php");
+		
+		$user = select_user($conn, $_SESSION["id"]) ;
+		
+		$favoris = explode('.', $user["favoris"]) ;
+		
+		if (count($favoris) < 2){
+			echo("<div id='quizz_name'>vous n'avez aucun favoris</div>");
+		} else {
+			foreach($favoris as $fav_id) {
+				if($fav_id != '') {
+					$qizz = select_quizz($conn, intval($fav_id)) ;
+					if(isset($qizz["image"])) {
+						$background = 'background-image: url("images/'.$qizz["image"].'");' ;
+					} else {
+						$background = "background-color: ".$qizz["color"].";";
+					}
+					echo "<a href='quizz.php?id=".$qizz["id"]."'  class='quizz' style='".$background."'><li><div class='nomQuizz'>".$qizz["name"]."</div></li></a>" ;
 				}
-				echo "<a href='quizz.php?id=".$qizz["id"]."'  class='quizz' style='".$background."'><li><div class='nomQuizz'>".$qizz["name"]."</div></li></a>" ;
 			}
 		}
+	} else {
+		echo "<h3>Veuillez vous connecter pour accéder aux favoris</h3>" ;
 	}
 	?>
 </ul>
--- mes_creation.php
+++ mes_creation.php
@@ -4,22 +4,26 @@ include("lib/head.php");
 
 <ul class="list_quizz">
 	<?php
-	include_once("crud/quizz.crud.php");
+	if(isset($_SESSION["id"])) {
+		include_once("crud/quizz.crud.php");
+		
+		$sql = "SELECT * FROM `quizz` WHERE `owner`=".$_SESSION['id'] ;
+		$result=mysqli_query($conn, $sql) ;
 	
-	$sql = "SELECT * FROM `quizz` WHERE `owner`=".$_SESSION['id'] ;
-	$result=mysqli_query($conn, $sql) ;
-
-	if (mysqli_num_rows($result) == 0){
-		echo("<div id='quizz_name'>vous n'avez créé aucun quizz</div>");
-	} else {
-		while ($row = mysqli_fetch_assoc($result)){
-			if(isset($row["image"])) {
-				$background = 'background-image: url("images/'.$row["image"].'");' ;
-			} else {
-				$background = "background-color: ".$row["color"].";";
-			}		
-			echo "<a href='quizz.php?id=".$row["id"]."'  class='quizz' style='".$background."'><li><div class='nomQuizz'>".$row["name"]."</div></li></a>" ;
+		if (mysqli_num_rows($result) == 0){
+			echo("<div id='quizz_name'>vous n'avez créé aucun quizz</div>");
+		} else {
+			while ($row = mysqli_fetch_assoc($result)){
+				if(isset($row["image"])) {
+					$background = 'background-image: url("images/'.$row["image"].'");' ;
+				} else {
+					$background = "background-color: ".$row["color"].";";
+				}		
+				echo "<a href='quizz.php?id=".$row["id"]."'  class='quizz' style='".$background."'><li><div class='nomQuizz'>".$row["name"]."</div></li></a>" ;
+			}
 		}
+	}  else {
+		echo "<h3>Veuillez vous connecter pour accéder à vos créations</h3>" ;
 	}
 	?>
 </ul>
