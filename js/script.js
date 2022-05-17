329de12aad5dd7d32ef06d1ca116cecbf8acc337
Osteriux <maziere.xavier@gmail.com>
2022-05-17 16:46:21 +0200

    update script fav

--- js/script.js
+++ js/script.js
@@ -23,3 +23,19 @@ async function clicMenu() {
 		menu = "close";
 	}
 }
+
+var icone_fav = document.querySelector(".icone_fav");
+
+icone_fav.addEventListener("click", clicfav);
+
+async function clicMenu() {
+	if (icone_fav.className == "icone_fav added") {
+		icone_fav.className = "icone_fav"
+		iconeMenu.style.backgroundImage = "url('/l1_info_4/Quizzy/images/fav_empty.png')";
+		await sleep(600);
+	} else {
+		icone_fav.className = "icone_fav added"
+		iconeMenu.style.backgroundImage = "url('/l1_info_4/Quizzy/images/fav_full.png')";
+		await sleep(600);
+	}
+}
