8ce073f72f1cad4496acdb36bc2967de8128c8c6
Osteriux <pixave73@gmail.com>
2022-05-17 14:38:43 +0200

    fix mes création link

--- lib/head.php
+++ lib/head.php
@@ -40,7 +40,7 @@
 					echo '<div id="cont_pic_menu" style="background-image: url(\'/l1_info_4/Quizzy/images/'.$image.'\');"></div><h4>Connecté en tant que : '.$user['nickname'].'</h4>';
 					echo '<a href="profil.php">profil</a>';
 					echo '<a href="favoris.php">Favoris</a>';
-					echo '<a href="mes_creation">Mes créations</a>';
+					echo '<a href="mes_creation.php">Mes créations</a>';
 					echo '<a href="#">Mes derniers Quizz</a>';
 					if ($_SESSION['admin']){
 						echo '<a href="admin/index.php">Accéder au dark web</a>';
