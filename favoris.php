d4cc564a2e0c095d63ef9d8f6bc4e57994a52bbf
Osteriux <pixave73@gmail.com>
2022-05-17 14:05:41 +0200

    fix favoris

--- favoris.php
+++ favoris.php
@@ -14,13 +14,15 @@ include("lib/head.php");
 		echo("<div id='quizz_name'>aucun favoris</div>");
 	} else {
 		foreach($favoris as $fav_id) {
-			$qizz = select_quizz($conn, intval($fav_id)) ;
-			if(isset($qizz["image"])) {
-				$background = 'background-image: url("images/'.$qizz["image"].'");' ;
-			} else {
-				$background = "background-color: ".$qizz["color"].";";
+			if($fav_id != '') {
+				$qizz = select_quizz($conn, intval($fav_id)) ;
+				if(isset($qizz["image"])) {
+					$background = 'background-image: url("images/'.$qizz["image"].'");' ;
+				} else {
+					$background = "background-color: ".$qizz["color"].";";
+				}
+				echo "<a href='quizz.php?id=".$qizz["id"]."'  class='quizz' style='".$background."'><li><div class='nomQuizz'>".$qizz["name"]."</div></li></a>" ;
 			}
-			echo "<a href='quizz.php?id=".$qizz["id"]."'  class='quizz' style='".$background."'><li><div class='nomQuizz'>".$qizz["name"]."</div></li></a>" ;
 		}
 	}
 	?>
