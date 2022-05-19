491398ccaac617cbf89c3d526402d39c24893fc2
P1xave <maziere.xavier@gmail.com>
2022-05-19 14:50:50 +0200

    test !??!,?

--- index.php
+++ index.php
@@ -17,18 +17,20 @@ include("lib/head.php");
 		$result = select_all_quizz($conn);
 	}
 	
-	
 	if (mysqli_num_rows($result) == 0){
 		echo("<div id='quizz_name'>Aucun résultat pour \"$rSearch\"</div>");
 	} else {
-		$result = array_reverse(mysqli_fetch_assoc($result), true) ;
-		foreach($result as $row){
-			if(isset($row["image"])) {
-				$background = 'background-image: url("images/'.$row["image"].'");' ;
+		while ($row = mysqli_fetch_assoc($result)){
+			$quizzs[] = $row ;
+		}
+		$quizzs = array_reverse($quizzs ,true);
+		foreach($quizzs as $quizz) {
+			if(isset($quizz["image"])) {
+				$background = 'background-image: url("images/'.$quizz["image"].'");' ;
 			} else {
-				$background = "background-color: ".$row["color"].";";
+				$background = "background-color: ".$quizz["color"].";";
 			}		
-			echo "<a href='quizz.php?id=".$row["id"]."'  class='quizz' style='".$background."'><li><div class='nomQuizz'>".$row["name"]."</div></li><img src='images/fav_empty.png' class='icone_fav'></a>" ;
+			echo "<a href='quizz.php?id=".$quizz["id"]."'  class='quizz' style='".$background."'><li><div class='nomQuizz'>".$quizz["name"]."</div></li><img src='images/fav_empty.png' class='icone_fav'></a>" ;
 		}
 	}
 	?>
