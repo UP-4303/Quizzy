3a711e72a966b033eb1c97a7c63a84b4b453dcf4
P1xave <maziere.xavier@gmail.com>
2022-05-19 22:55:14 +0200

    test

--- index.php
+++ index.php
@@ -4,11 +4,11 @@ include("lib/head.php");
 if(isset($_SESSION["id"])) {
 	$user = select_user($conn, $_SESSION["id"]) ;
 	$fav = $user["favoris"] ;
-	print("étape1") ;
+	print_r($_POST) ;
 	if(isset($_post["not_added"])) {
+		print($_post["not_added"]) ;
 		$fav = ".".$_post["not_added"].$fav ;
 		update_quizz_favoris($conn, $_SESSION["id"], $fav) ;
-		print("add") ;
 		unset($_post["not_added"]) ;
 	}
 	if(isset($_post["remove"])) {
